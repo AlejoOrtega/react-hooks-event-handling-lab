9ea9610cba50c9d99224ffa00904e45e336b599a
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2022-04-26 18:46:35 -0400

    commit tree 1065e1a8cf9036009dc416ecd427bbf4101e9442

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+// Code EyesOnMe Components
+import React from 'react';
+
+function EyesOnMe(props) {
+    const onFocusAction = () => {
+        console.log('Good!')
+    }
+
+    const onBlurAction = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    
+    return (
+        <>
+            <button onFocus={onFocusAction} onBlur={onBlurAction}>Eyes on me</button>
+        </>
+    );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function Keypad() {
+    const onPasswordChange = () => {
+        console.log('Entering password...')
+    }
+    return (
+        <>
+            <input onChange={onPasswordChange} type='password'/>
+        </>
+    );
+}
+
+export default Keypad;
